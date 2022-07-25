e45c469ebed9cfec4e2965957ab819e325b5e043
='Johnson-ad <=>
2022-07-25 16:08:05 +0000

    Frontend, creation du component navigation

--- server/app/Http/Controllers/ProduitController.php
+++ server/app/Http/Controllers/ProduitController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Produit;
 
 class ProduitController extends Controller
 {
-    //
+    public function index()
+    {
+        // $porduit = Produit::all();
+        // dd($porduit);
+        // return view('produit.index');
+    }
 }
