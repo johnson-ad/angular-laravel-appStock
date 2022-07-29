7cb6519ce318dd5e9fd0a5e2c6bb2bb1f7f7ae5a
='Johnson-ad <=>
2022-07-29 12:50:35 +0000

    Frontend, passage des data d un composant parent a un composant enfant

--- clients/src/app/app.component.ts
+++ clients/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Like } from '../app/models/like';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  myLike!: Like;
 
+  ngOnInit() {
+    this.myLike = new Like(
+      'Johnson & Johnson',
+      'Johnson & Johnson is a global leadere beauty brand in the U.S.',
+      new Date(),
+      0,
+      'https://images.unsplash.com/photo-1589156229687-496a31ad1d1f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80'
+    );
+  }
 }
+
+
---
+++ clients/src/app/models/like.ts
@@ -0,0 +1,25 @@
+
+export class Like {
+    //Premiere methode
+    // title: string;
+    // description: string;
+    // createDate: Date;
+    // like: number;
+    // imageUrl: string;
+
+    // constructor(title: string, description: string, createDate: Date, like: number, imageUrl: string) {
+    //     this.title = title;
+    //     this.description = description;
+    //     this.createDate = createDate;
+    //     this.like = like;
+    //     this.imageUrl = imageUrl;
+    // }
+
+    //Deuxieme methode
+    constructor(
+        public title: string,
+        public description: string,
+        public createDate: Date,
+        public like: number,
+        public imageUrl: string) { }
+}
